90fa76268074b585087b52b80cb5d39176c4959e
itggot-Romina-Asadi <romina.asadi@itggot.se>
2017-11-23 11:04:40 +0100

    Startat login

--- todoapp/app.rb
+++ todoapp/app.rb
@@ -1,6 +1,4 @@
 class App < Sinatra::Base
-
-	
 	enable :sessions
 
 	get '/' do
@@ -11,15 +9,35 @@ class App < Sinatra::Base
 		slim(:login)
 	end
 
+	post '/login' do
+		db = SQLite3::Database.new("todoapp.sqlite")
+		username = params["username"]
+		password = params["password"]
+		if password == db.execute("SELECT password FROM login WHERE username=?", [username])
+			session[:username] = username
+			redirect('/')
+		end
+	
+
+		# db.execute("SELECT username WHERe id=i")
+	end
+
 	get '/register' do
 		slim (:register)
 	end
 
 	post '/register' do
-		db = SQLite::Database.new("todoapp.sqlite")
-		username = params[:username]
-		password = params[:passsword]
-		db.execute("INSERT INTO login (username, password) VALUES (?, ?)", [username,password])
+		db = SQLite3::Database.new("todoapp.sqlite")
+		username = params["username"]
+		password = params["password"]
+		password2 = params["password2"]
+		# Kryptera lösenord
+		if password == password2
+			db.execute("INSERT INTO login (username, password) VALUES (?, ?)", [username,password])
+		else
+			raise ArgumentError
+			# Fixa argumenterror
+		end
 	end
 
 end           
