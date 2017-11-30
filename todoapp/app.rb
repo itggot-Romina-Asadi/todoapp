5f0c0a234e354b5e16a12c2f14317929546968fa
itggot-Romina-Asadi <romina.asadi@itggot.se>
2017-11-30 11:03:26 +0100

    Mer felhantering

--- todoapp/app.rb
+++ todoapp/app.rb
@@ -25,10 +25,12 @@ class App < Sinatra::Base
 		db = SQLite3::Database.new("todoapp.sqlite")
 		username = params["username"]
 		password = params["password"]
+		user_id = db.execute("SELECT id FROM login WHERE username='#{username}'")
 		password_digest = db.execute("SELECT password FROM login WHERE username='#{username}'").join
 		password_digest = BCrypt::Password.new(password_digest)
 		if password_digest == password
 			session[:username] = username
+			session[:id] = user_id
 			redirect('/todo')
 		else
 			redirect('/register')
@@ -41,17 +43,29 @@ class App < Sinatra::Base
 		password = params["password"]
 		password2 = params["password2"]
 		password_digest = BCrypt::Password.create("#{password}")
-		if password == password2
-			begin
-				db.execute("INSERT INTO login (username, password) VALUES (?, ?)", [username,password_digest])
-			rescue
-				session[:message] = "The username is unavailable "
+		if username.length > 0
+			if password == password2 && password.length > 0
+				begin
+					db.execute("INSERT INTO login (username, password) VALUES (?, ?)", [username,password_digest])
+				rescue
+					session[:message] = "The username is unavailable"
+					redirect('/error')
+				end
+				redirect('/login')
+			else
+				session[:message] = "Password unavailable"
 				redirect('/error')
 			end
-			redirect('/login')
 		else
-			session[:message] = "Passwords doesn't match"
+			session[:message] = "The username is unavailable"
 			redirect('/error')
 		end
+	end
+
+	post '/todo' do
+		db = SQLite3::Database.new("todoapp.sqlite")
+		todo = params["todo"]
+		db.execute("INSERT INTO todo (todo, user_id) VALUES (?, ?)", [todo, session[:id]])
+		redirect('/todo')
 	end
 end           
